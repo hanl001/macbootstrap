ed46b98870f42204e29a9b4ebeb39174e76b5c97
hanling <hanling1217@gmail.com>
2019-06-24 17:56:03 +0800

    change manager.sh to link.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ function exe_cmd() {
     echo $1
     eval $1
 }
+
 xcode-select --install
 git clone "https://github.com/hanl001/macbootstrap" ~/.macbootstrap
 exe_cmd "cd ~/.macbootstrap"
 exe_cmd "bash install.sh"
-sh manager.sh relink
+exe_cmd "bash link.sh"
