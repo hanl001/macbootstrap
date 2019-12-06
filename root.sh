6f8d96cc073aadd81c17dca62515d3b7650403a9
hanl001 <hanling1217@gmail.com>
2019-12-06 15:03:08 +0800

    add export .sh

--- root.sh
+++ root.sh
@@ -3,4 +3,5 @@ source $MACBOOTSTRAP_ROOT/sh-config/config.sh
 source $MACBOOTSTRAP_ROOT/sh-config/custom.sh
 source $MACBOOTSTRAP_ROOT/sh-config/proxy.sh
 source $MACBOOTSTRAP_ROOT/sh-config/search.sh
+source $MACBOOTSTRAP_ROOT/sh-config/export.sh
 
