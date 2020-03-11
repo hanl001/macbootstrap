9f775aed3aa478e3dddb1b1056b21821a6dca65e
hanl001 <hanling1217@gmail.com>
2020-03-11 19:26:06 +0800

    typo

--- sh-config/export.sh
+++ sh-config/export.sh
@@ -1,5 +1,3 @@
 ## flutter env
 export PATH="$PATH:Users/hanling/Documents/flutter/bin"
-export PUB_HOSTED_URL=https://pub.flutter-io.cn
-export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
 
