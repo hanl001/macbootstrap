2cb43125401af162ae4c1216fd8c8f3f81d77cad
hanling <hanling1217@gmail.com>
2021-05-25 13:47:42 +0800

    use ssh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ function exe_cmd() {
 }
 
 xcode-select --install
-git clone "https://github.com/hanl001/macbootstrap" ~/.macbootstrap
+git clone "git@github.com:hanl001/macbootstrap.git" ~/.macbootstrap
 exe_cmd "cd ~/.macbootstrap"
 exe_cmd "bash install.sh"
 exe_cmd "bash link.sh"
--- sh-config/export.sh
+++ sh-config/export.sh
@@ -1,5 +1,5 @@
 ## flutter env
 export PATH="$PATH:Users/hanling/Documents/flutter/bin"
-
+## arc env
 export PATH="$PATH:$HOME/phabricator/arcanist/bin"
 
