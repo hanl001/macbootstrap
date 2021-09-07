67c06ca6d4118efe94ffdb5409e23d49528518c0
hanl001 <hanling1217@gmail.com>
2021-09-07 23:13:42 +0800

    remove unused backup

--- link.sh
+++ link.sh
@@ -14,19 +14,14 @@ function exe_cmd()
 }
 
 # link git config
-exe_cmd "mv ~/.gitconfig ~/.gitconfig_backup"
 exe_cmd "ln -sf ~/.macbootstrap/git-config/_gitconfig ~/.gitconfig"
 exe_cmd "ln -sf ~/.macbootstrap/git-config/_gitignore_global ~/.gitignore_global"
 
 # link shell config
-exe_cmd "mv ~/.zshrc ~/.zshrc_backup"
 exe_cmd "ln -sf ~/.macbootstrap/sh-config/_zshrc ~/.zshrc"
 
-exe_cmd "mv ~/.bashrc ~/.bashrc_backup"
 exe_cmd "ln -sf ~/.macbootstrap/sh-config/_bashrc ~/.bashrc"
 
-exe_cmd "mv ~/.bash_profile  ~/.bash_profile_backup"
 exe_cmd "ln -sf ~/.macbootstrap/sh-config/_bash_profile ~/.bash_profile" 
 
-exe_cmd "mv ~/.inputrc  ~/.inputrc_backup"
 exe_cmd "ln -sf ~/.macbootstrap/sh-config/_inputrc ~/.inputrc"
